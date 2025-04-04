443ab1fa4ff314116570387c6e408f32ebffda78
Nirmal-Singh-G <nirmalsinghg.10@gmail.com>
2025-04-04 18:54:09 +0530

    four methods added

--- src/org/com/Class1.java
+++ src/org/com/Class1.java
@@ -6,5 +6,30 @@ public class Class1 {
 		// TODO Auto-generated method stub
 
 	}
+	
+	private void fastFood() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void southIndia() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void northIndia() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void briyani() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void grill() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
